4df548c7f1ad4bf72b830eee55e1e633e25d300c
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-06-19 17:00:06 +0200

    add api calls

---
+++ apps/ui/src/utils/api.ts
@@ -0,0 +1,158 @@
+import secretariumHandler from '@/utils/secretarium-handler';
+import { TransactionResult, TokenIdentityResult, FileUploadTokenResult, ListDataRoomsResult } from './types';
+
+export const klaveContract = import.meta.env.VITE_APP_KLAVE_CONTRACT;
+
+export const waitForConnection = () =>
+    new Promise<void>((resolve) => {
+        const loopCondition = () => {
+            const isConnected = secretariumHandler.isConnected();
+            if (isConnected) resolve();
+            else setTimeout(loopCondition, 1000);
+        };
+        loopCondition();
+    });
+
+export const setTokenIdentity = async (token: string): Promise<TransactionResult> =>
+    waitForConnection()
+        .then(() =>
+            secretariumHandler.request(
+                klaveContract,
+                'setTokenIdentity',
+                { token },
+                `setTokenIdentity-${Math.random()}`
+            )
+        )
+        .then(
+            (tx) =>
+                new Promise((resolve, reject) => {
+                    tx.onResult((result) => {
+                        resolve(result);
+                    });
+                    tx.onError((error) => {
+                        reject(error);
+                    });
+                    tx.send().catch(reject);
+                })
+        );
+
+export const setWebServerIdentity = async (spkiPublicKey: string): Promise<TransactionResult> =>
+    waitForConnection()
+        .then(() =>
+            secretariumHandler.request(
+                klaveContract,
+                'setWebServerIdentity',
+                { spkiPublicKey },
+                `setWebServerIdentity-${Math.random()}`
+            )
+        )
+        .then(
+            (tx) =>
+                new Promise((resolve, reject) => {
+                    tx.onResult((result) => {
+                        resolve(result);
+                    });
+                    tx.onError((error) => {
+                        reject(error);
+                    });
+                    tx.send().catch(reject);
+                })
+        );
+
+export const createDataRoom = async (dataRoomId: string): Promise<TransactionResult> =>
+    waitForConnection()
+        .then(() =>
+            secretariumHandler.request(
+                klaveContract,
+                'createDataRoom',
+                { dataRoomId },
+                `createDataRoom-${Math.random()}`
+            )
+        )
+        .then(
+            (tx) =>
+                new Promise((resolve, reject) => {
+                    tx.onResult((result) => {
+                        resolve(result);
+                    });
+                    tx.onError((error) => {
+                        reject(error);
+                    });
+                    tx.send().catch(reject);
+                })
+        );
+
+export const updateDataRoom = async (dataRoomId: string): Promise<TransactionResult> =>
+    waitForConnection()
+        .then(() =>
+            secretariumHandler.request(
+                klaveContract,
+                'updateDataRoom',
+                { dataRoomId },
+                `updateDataRoom-${Math.random()}`
+            )
+        )
+        .then(
+            (tx) =>
+                new Promise((resolve, reject) => {
+                    tx.onResult((result) => {
+                        resolve(result);
+                    });
+                    tx.onError((error) => {
+                        reject(error);
+                    });
+                    tx.send().catch(reject);
+                })
+        );
+
+export const getTokenIdentity = async (): Promise<TokenIdentityResult> =>
+    waitForConnection()
+        .then(() =>
+            secretariumHandler.request(klaveContract, 'getTokenIdentity', {}, `getTokenIdentity-${Math.random()}`)
+        )
+        .then(
+            (tx) =>
+                new Promise((resolve, reject) => {
+                    tx.onResult((result) => {
+                        resolve(result);
+                    });
+                    tx.onError((error) => {
+                        reject(error);
+                    });
+                    tx.send().catch(reject);
+                })
+        );
+
+export const getFileUploadToken = async (): Promise<FileUploadTokenResult> =>
+    waitForConnection()
+        .then(() =>
+            secretariumHandler.request(klaveContract, 'getFileUploadToken', {}, `getFileUploadToken-${Math.random()}`)
+        )
+        .then(
+            (tx) =>
+                new Promise((resolve, reject) => {
+                    tx.onResult((result) => {
+                        resolve(result);
+                    });
+                    tx.onError((error) => {
+                        reject(error);
+                    });
+                    tx.send().catch(reject);
+                })
+        );
+
+export const listDataRooms = async (): Promise<ListDataRoomsResult> =>
+    waitForConnection()
+        .then(() => secretariumHandler.request(klaveContract, 'listDataRooms', {}, `listDataRooms-${Math.random()}`))
+        .then(
+            (tx) =>
+                new Promise((resolve, reject) => {
+                    tx.onResult((result) => {
+                        resolve(result);
+                    });
+                    tx.onError((error) => {
+                        reject(error);
+                    });
+                    tx.send().catch(reject);
+                })
+        );
--- apps/ui/src/utils/types.ts
+++ apps/ui/src/utils/types.ts
@@ -4,3 +4,43 @@ export type KeyPair = EncryptedKeyPair & {
     id: string;
     name: string;
 };
+
+export type TransactionResult =
+    | {
+          success: true;
+          message: string;
+      }
+    | {
+          success: false;
+          exception: string;
+      };
+
+export type TokenIdentityResult =
+    | {
+          success: true;
+          identity: string;
+      }
+    | {
+          success: false;
+          exception: string;
+      };
+
+export type FileUploadTokenResult =
+    | {
+          success: true;
+          token: string;
+      }
+    | {
+          success: false;
+          exception: string;
+      };
+
+export type ListDataRoomsResult =
+    | {
+          success: true;
+          dataRooms: string[];
+      }
+    | {
+          success: false;
+          exception: string;
+      };
