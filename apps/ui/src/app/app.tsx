597637a390bf09910a9d361e152246bddad759fd
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-06-19 15:07:43 +0200

    setup shadcn/ui

--- apps/ui/src/app/app.tsx
+++ apps/ui/src/app/app.tsx
@@ -1,9 +1,11 @@
 import NxWelcome from './nx-welcome';
+import { Button } from '@klave-secure-rooms/ui-kit/ui';
 
 export function App() {
   return (
     <div>
       <NxWelcome title="ui" />
+      <Button>Hello World</Button>
     </div>
   );
 }
---
+++ libs/ui-kit/util/README.md
@@ -0,0 +1,7 @@
+# ui-kit-util
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build ui-kit-util` to build the library.
