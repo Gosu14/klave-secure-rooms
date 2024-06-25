01d2ad9d1071acbcdf144ca62006bbe78e09797e
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-06-25 08:24:24 +0200

    add form component

--- libs/ui-kit/ui/src/index.ts
+++ libs/ui-kit/ui/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/ui/button';
 export * from './lib/ui/card';
 export * from './lib/ui/input';
 export * from './lib/ui/sonner';
+export * from './lib/ui/form';
+export * from './lib/ui/label';
