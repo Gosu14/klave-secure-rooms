cb113e7ce253e4a97f49a28f3c2d372add8137f1
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-06-25 08:11:11 +0200

    add helper functions

--- apps/ui/src/utils/helpers.ts
+++ apps/ui/src/utils/helpers.ts
@@ -1,7 +1,33 @@
-export function urlToId(id: string) {
+export const urlToId = (id: string) => {
     return id.replace(/-/g, '+').replace(/_/g, '/');
-}
+};
 
-export function idToUrl(id: string) {
+export const idToUrl = (id: string) => {
     return id.replace(/\+/g, '-').replace(/\//g, '_');
-}
+};
+
+export const subtleHash = async (message: string | undefined) => {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(message);
+    const hash = await crypto.subtle.digest('SHA-256', data);
+    return hash;
+};
+
+export const arrayBufferToBase64 = (buffer: ArrayBuffer) => {
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return btoa(binary);
+};
+
+export const base64ToArrayBuffer = (base64: string) => {
+    const binaryString = atob(base64);
+    const bytes = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+};
