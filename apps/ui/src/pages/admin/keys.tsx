97e0a386cb22a375510a970b8a12c7a32236ba8d
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-06-27 15:32:55 +0200

    add a /keys page for key management

--- apps/ui/src/pages/admin/keys.tsx
+++ apps/ui/src/pages/admin/keys.tsx
@@ -1,7 +1,22 @@
 import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { resetIdentities } from '../../utils/api';
+import { exportWebServerIdentity, isConnected, resetIdentities } from '../../utils/api';
 import { Button } from '@klave-secure-rooms/ui-kit/ui';
+import { redirect } from 'react-router-dom';
+
+export const loader = async () => {
+    const isConnectedState = isConnected();
+
+    if (!isConnectedState) {
+        return redirect('/auth');
+    }
+
+    const res = await exportWebServerIdentity('raw');
+
+    console.log(res);
+
+    return null;
+};
 
 export const Keys = () => {
     const [error, setError] = useState<string | null>(null);
@@ -28,7 +43,9 @@ export const Keys = () => {
             <div className="flex flex-col gap-4 px-12 py-4">
                 <h2 className="text-lg font-semibold">Keys</h2>
                 <div className="flex flex-col gap-2">
-                    <Button className="w-48" onClick={handleResetIdentities}>Reset Identities</Button>
+                    <Button className="w-48" onClick={handleResetIdentities}>
+                        Reset Identities
+                    </Button>
                 </div>
                 {error && <p className="overflow-clip text-red-500">{error}</p>}
                 {successMsg && <p className="overflow-clip text-green-500">{successMsg}</p>}
