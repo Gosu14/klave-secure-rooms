d4b18e7e9cda843810db5b6e6afe6b87a9345312
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-06-27 10:38:32 +0200

    add logout button

--- apps/ui/src/components/header.tsx
+++ apps/ui/src/components/header.tsx
@@ -1,14 +1,26 @@
-import { FileLock2 } from 'lucide-react';
-import { Link } from 'react-router-dom';
+import { Button } from '@klave-secure-rooms/ui-kit/ui';
+import { FileLock2, LogOut } from 'lucide-react';
+import { Link, useNavigate } from 'react-router-dom';
+import secretariumHandler from '../utils/secretarium-handler';
 
 export const Header = () => {
+    const navigate = useNavigate();
+
     return (
         <header className="sticky top-0 w-full border-b border-slate-200 bg-slate-50">
-            <div className="flex px-12 py-4">
+            <div className="flex justify-between px-12 py-4">
                 <Link to="/" className="flex items-center gap-2 transition-colors hover:text-slate-500">
                     <FileLock2 />
                     <span className="text-xl font-semibold">Secure Data Rooms</span>
                 </Link>
+                <Button
+                    onClick={() => secretariumHandler.disconnect().then(() => navigate('/auth'))}
+                    variant="ghost"
+                    className="mt-auto flex items-center gap-2 rounded-lg p-2 text-xl font-semibold transition-colors hover:bg-blue-100 hover:text-blue-500"
+                >
+                    <LogOut />
+                    Logout
+                </Button>
             </div>
         </header>
     );
