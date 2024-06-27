09eb3e6dfd4389b3b4c681c63bbbdd58a14a3060
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-06-27 12:51:19 +0200

    add call to reset backend and webserver identities

--- apps/ui/src/pages/admin/keys.tsx
+++ apps/ui/src/pages/admin/keys.tsx
@@ -1,6 +1,25 @@
+import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { resetIdentities } from '../../utils/api';
+import { Button } from '@klave-secure-rooms/ui-kit/ui';
 
 export const Keys = () => {
+    const [error, setError] = useState<string | null>(null);
+    const [successMsg, setSuccessMsg] = useState<string | null>(null);
+
+    const handleResetIdentities = async () => {
+        const result = await resetIdentities({ resetBackend: true, resetWebServer: true });
+        console.log(result);
+
+        if (result.success) {
+            setSuccessMsg(result.message);
+            setError(null);
+        } else {
+            setError(result.message);
+            setSuccessMsg(null);
+        }
+    };
+
     return (
         <>
             <Helmet>
@@ -8,6 +27,11 @@ export const Keys = () => {
             </Helmet>
             <div className="flex flex-col gap-4 px-12 py-4">
                 <h2 className="text-lg font-semibold">Keys</h2>
+                <div className="flex flex-col gap-2">
+                    <Button className="w-48" onClick={handleResetIdentities}>Reset Identities</Button>
+                </div>
+                {error && <p className="overflow-clip text-red-500">{error}</p>}
+                {successMsg && <p className="overflow-clip text-green-500">{successMsg}</p>}
             </div>
         </>
     );
