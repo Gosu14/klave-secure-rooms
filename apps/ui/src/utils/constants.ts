7dd54dc93e7f4a2e190b7c76a07cd84f36d37fee
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-07-04 11:19:04 +0200

    change localStorage key name

--- apps/ui/src/utils/constants.ts
+++ apps/ui/src/utils/constants.ts
@@ -1,3 +1,3 @@
 // TODO : This is for demo purpose only, do not use this in production
 export const ENC_PWD = 'demoKlave';
-export const LOC_KEY = 'demo:portalEncKey';
+export const LOC_KEY = 'demo:secureDataRoomEncKey';
