2b7172522d3028aac5c2e4c30fa04c0214e3116a
Florian Guitton <2495155+fguitton@users.noreply.github.com>
2024-07-04 13:42:23 +0100

    chore: Switch to standard fs

--- apps/uploader/src/utils/keyHolder.ts
+++ apps/uploader/src/utils/keyHolder.ts
@@ -1,4 +1,4 @@
-import fs from 'node:fs/promises';
+import fs from 'node:fs';
 import path from 'node:path';
 import { subtle } from 'node:crypto';
 import { ClearKeyPair, Key, SCP, Utils } from '@secretarium/connector';
@@ -6,7 +6,8 @@ import { ClearKeyPair, Key, SCP, Utils } from '@secretarium/connector';
 const idKeyName = process.env.IKEY ?? 'i.key.pem';
 const signKeyName = process.env.SKEY ?? 's.key.pem';
 const roomKeyName = process.env.RKEY ?? 'r.key.pem';
-const deploymentId = process.env.DID ?? 'demo-room';
+const deploymentId = process.env.DID ?? process.env.NX_APP_KLAVE_CONTRACT ?? 'demo-room';
+const secretariumEndpoint = process.env.NX_APP_SECRETARIUM_GATEWAYS?.split('#')?.[2] ?? 'wss://klave-prod.secretarium.org';
 
 const klave = new SCP({
     logger: console
@@ -18,19 +19,15 @@ export class KeyHolder {
     static roomKey?: CryptoKey;
     static async loadServerIdentity() {
         const keysPath = path.resolve(path.join(__dirname, 'keys'));
-        await fs.mkdir(keysPath, { recursive: true });
-
+        fs.mkdirSync(keysPath, { recursive: true});
         const idKeyPath = path.resolve(keysPath, idKeyName);
-        const idKeyStat = await fs.stat(idKeyPath).catch(() => {
-            console.warn('Klave identity is missing');
-        });
-        if (!idKeyStat?.isFile) {
+        if (!fs.existsSync(idKeyPath)) {
             console.info('Generating Klave identity key...');
             KeyHolder.idKey = await Key.createKey();
-            await fs.writeFile(idKeyPath, JSON.stringify(await KeyHolder.idKey.exportKey()));
+            await fs.writeFileSync(idKeyPath, JSON.stringify(await KeyHolder.idKey.exportKey()));
         } else {
             console.info('Loading Klave indentity key...');
-            const idKeyContent = await fs.readFile(idKeyPath);
+            const idKeyContent = fs.readFileSync(idKeyPath);
             const idKey = JSON.parse(idKeyContent.toString()) as ClearKeyPair;
             KeyHolder.idKey = await Key.importKey(idKey);
         }
@@ -38,28 +35,27 @@ export class KeyHolder {
         klave.onError((e) => {
             console.error(e);
         });
-        await klave.connect('wss://klave.secretarium.org', KeyHolder.idKey);
-
+        await klave.connect(secretariumEndpoint, KeyHolder.idKey);
         const signKeyPath = path.resolve(keysPath, signKeyName);
-        const signKeyStat = await fs.stat(signKeyPath).catch(() => {
-            return;
-        });
-        if (!signKeyStat?.isFile) {
+        if (!fs.existsSync(signKeyPath)) {
             // throw new Error('The signing key must be a file');
             const tx = klave.newTx(deploymentId, 'exportStorageServerPrivateKey', undefined, { format: 'raw' });
             const newSignKeyB64: string = await new Promise((resolve, reject) => {
                 tx.onResult((result) => {
+                    console.log('result', result)
                     if (result.success) return resolve(result.message);
                     reject();
                 });
-                tx.onError(() => {
+                tx.onError((error) => {
+                    console.error('error',error)
                     reject();
                 });
+                tx.send();
             });
             const newSignKeyContent = Utils.fromBase64(newSignKeyB64);
-            await fs.writeFile(signKeyPath, newSignKeyContent);
+             fs.writeFileSync(signKeyPath, newSignKeyContent);
         }
-        const signKeyContent = await fs.readFile(signKeyPath);
+        const signKeyContent =  fs.readFileSync(signKeyPath);
         KeyHolder.signKey = await subtle.importKey(
             'raw',
             signKeyContent,
@@ -71,10 +67,7 @@ export class KeyHolder {
         );
 
         const roomKeyPath = path.resolve(keysPath, roomKeyName);
-        const roomKeyStat = await fs.stat(roomKeyPath).catch(() => {
-            return;
-        });
-        if (!roomKeyStat?.isFile) {
+        if (!fs.existsSync(roomKeyPath)) {
             // throw new Error('The signing key must be a file');
             const tx = klave.newTx(deploymentId, 'getPublicKeys');
             const newRoomKeyPEM: string = await new Promise((resolve, reject) => {
@@ -85,10 +78,11 @@ export class KeyHolder {
                 tx.onError(() => {
                     reject();
                 });
+                tx.send();
             });
-            await fs.writeFile(roomKeyPath, newRoomKeyPEM);
+             fs.writeFileSync(roomKeyPath, newRoomKeyPEM);
         }
-        const roomKeyContent = await fs.readFile(roomKeyPath);
+        const roomKeyContent =  fs.readFileSync(roomKeyPath);
         KeyHolder.roomKey = await subtle.importKey(
             'spki',
             roomKeyContent,
